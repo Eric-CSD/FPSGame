7f0bfafa68d2e35d0738b339460a95e59ace6391
Shiva Bindraban <shiva.bindraban@gmail.com>
2020-09-16 14:03:47 +0200

    Added a 2nd robotkiller

--- FPSMicrogame/Assets/FPS/Scripts/DoorTrigger.cs
+++ FPSMicrogame/Assets/FPS/Scripts/DoorTrigger.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class DoorTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject Enemy1;
+    [SerializeField] private GameObject Enemy2;
     bool isOpened = false;
     private Animator animator;
 
@@ -14,8 +15,8 @@ public class DoorTrigger : MonoBehaviour
 
     private void Update()
     {
-      if (Enemy1 == null && isOpened ==false){
-        Debug.Log("B");
+      if (Enemy1 == null && Enemy2 == null && isOpened ==false){
+        Debug.Log("Test 2 enemies");
         isOpened = true;
         animator.SetBool("Open", true);
       }
