c4ad520b66f882e72d8ccf33a16d2b5f5df37af8
Eric Schuster <eric.schuster@hotmail.com>
2020-09-16 23:52:52 +0200

    Improved lighting, added first sliding door & rope platform

--- FPSMicrogame/Assets/FPS/Scripts/DoorTrigger.cs
+++ FPSMicrogame/Assets/FPS/Scripts/DoorTrigger.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 public class DoorTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject Enemy1;
-    [SerializeField] private GameObject Enemy2;
+    // [SerializeField] private GameObject Enemy2;
     bool isOpened = false;
     private Animator animator;
 
     private void Awake(){
       animator = GetComponent<Animator>();
-      animator.SetBool("Open", false);
+      animator.SetBool("Open1", false);
     }
 
     private void Update()
     {
-      if (Enemy1 == null && Enemy2 == null && isOpened ==false){
+      if (Enemy1 == null && isOpened ==false){
         Debug.Log("GitHubPushPullTest");
         isOpened = true;
-        animator.SetBool("Open", true);
+        animator.SetBool("Open1", true);
       }
 
     }
